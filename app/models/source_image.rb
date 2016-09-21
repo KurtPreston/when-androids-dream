8647e582bb6adc73818306febcbc8da2f5b82858
Kurt Preston <github@kurtpreston.com>
2016-09-21 13:17:25 -0500

    Style tweaks

--- app/models/source_image.rb
+++ app/models/source_image.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 
 class SourceImage < ApplicationRecord
-  has_attached_file :image, styles: { medium: '975x975>', thumb: '150x150>' }
+  has_attached_file :image, styles: { medium: '875x875>', thumb: '150x150>' }
   validates_attachment :image,
     presence: true,
     size: { in: 0..20.megabytes },
