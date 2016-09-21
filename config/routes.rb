f3bbdbd82f29abe0aebcb107f95a49357da5bbb6
Kurt Preston <github@kurtpreston.com>
2016-09-21 09:58:40 -0500

    Applying some basic page styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :generated_images
+  resources :generated_images, path: '/'
   resources :source_images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
